47022ecefbdaf2d63df30b4ae98031783fdc02fb
Dheeraj <Dheeraj@Users-MacBook-Pro.local>
2019-03-17 13:18:49 -0500

    Updates

---
+++ precache-manifest.2bda78a3549fc4b22a238c9c1125c4c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "570cc7e2acc88cbc4c34",
+    "url": "/Video-search/static/js/runtime~main.1fa73ddd.js"
+  },
+  {
+    "revision": "23681eadc42f42916d73",
+    "url": "/Video-search/static/js/main.5d9a8a0c.chunk.js"
+  },
+  {
+    "revision": "e053a1da17cbfeebd8e3",
+    "url": "/Video-search/static/js/2.9176d4f9.chunk.js"
+  },
+  {
+    "revision": "23681eadc42f42916d73",
+    "url": "/Video-search/static/css/main.5913a198.chunk.css"
+  },
+  {
+    "revision": "a857c193a92445b44ae233d6e20f934d",
+    "url": "/Video-search/index.html"
+  }
+];
